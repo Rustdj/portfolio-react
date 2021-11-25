fd85d79986bdad5a92629a323d13901c2aa6792e
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-11-25 12:55:41 +0500

    create new component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './header.modules.css';
 import  logo  from './logo.jpeg';
 import { NavLink } from 'react-router-dom';
+import react from '../skills/skills/react.png';
+import js from '../skills/skills/js.png';
+import css from '../skills/skills/css.png';
+import html from '../skills/skills/html.png';
 
 
 
@@ -10,6 +14,10 @@ const Header = () => {
     return (
         <div className='headerTop'>  
             <NavLink to='/home'><img src={logo}/></NavLink>
+            <img src={react}/>
+            <img src={js}/>
+            <img src={css}/>
+            <img src={html}/>
         </div>
     )
 }
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import './portfolio.modules.css';
+import one from './photo/one.png';
+import two from './photo/two.png';
+import three from './photo/three.png';
+import four from './photo/four.png';
+import five from './photo/five.png';
+import six from './photo/six.png';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -15,36 +22,24 @@ const Portfolio = () => {
                 </div>
                 <div className='portfolio__items'>
                     <div className='portfolio__wrapper'>
-                        <a><img src='#'  alt='#'/></a>
+                            <NavLink to='/asdasd'><img src={one}  alt='work'/></NavLink>
                     </div>
+                        <div className='portfolio__wrapper'>
+                                <a href='#'><img src={two}  alt='work'/></a>
+                        </div>
+                            <div className='portfolio__wrapper'>
+                                    <a href='#'><img src={three}  alt='work'/></a>
+                            </div>
+                                <div className='portfolio__wrapper'>
+                                        <a href='#'><img src={four}  alt='work'/></a>
+                                </div>
+                            <div className='portfolio__wrapper'>
+                                    <a href='#'><img src={five}  alt='work'/></a>
+                            </div>
+                        <div className='portfolio__wrapper'>
+                                <a href='#'><img src={six}  alt='work'/></a>
+                        </div>
                 </div>
-                <div className='portfolio__items'>
-                    <div className='portfolio__wrapper'>
-                        <a><img src='#'  alt='#'/></a>
-                    </div>
-                </div>
-                <div className='portfolio__items'>
-                    <div className='portfolio__wrapper'>
-                        <a><img src='#'  alt='#'/></a>
-                    </div>
-                </div>
-                <div className='portfolio__items'>
-                    <div className='portfolio__wrapper'>
-                        <a><img src='#'  alt='#'/></a>
-                    </div>
-                </div>
-                <div className='portfolio__items'>
-                    <div className='portfolio__wrapper'>
-                        <a><img src='#'  alt='#'/></a>
-                    </div>
-                </div>
-                <div className='portfolio__items'>
-                    <div className='portfolio__wrapper'>
-                        <a><img src='#'  alt='#'/></a>
-                    </div>
-                </div>
-                
-            
         </div>
     )
 }
